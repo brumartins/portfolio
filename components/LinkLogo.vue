6d6af846770c0ad6747c759239c491e9d5d12bc9
brumartins <46678131+brumartins@users.noreply.github.com>
2020-12-14 21:17:59 +0100

    feat: add multiple logos as components for social networks

---
+++ components/LinkLogo.vue
@@ -0,0 +1,9 @@
+<template>
+  <a
+    target="_blank"
+    rel="noopener noreferrer"
+    class="hover:opacity-50 transition-all duration-500 ease-in-out"
+  >
+    <slot></slot>
+  </a>
+</template>
